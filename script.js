5f315d23add613d0588c321d6eea0214e5994929
paragsuri05-ai <paragsuri05@gmail.com>
2026-01-26 14:01:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const cards=document.querySelectorAll('.card');
+window.addEventListener('scroll',()=>{
+cards.forEach(c=>{
+if(c.getBoundingClientRect().top < window.innerHeight-80){
+c.classList.add('show');
+}
+});
+});
